634d22bc4353656f6cc04112ef8d0ae080617948
vtrv101 <vlad@vlads-macbook-pro.local>
2009-03-16 10:22:43 -0700

    Created system api-s sample app

---
+++ app/Camera/controller.rb
@@ -0,0 +1,34 @@
+require 'rho/rhocontroller'
+
+class CameraController < Rho::RhoController
+  @layout = :simplelayout
+  
+  def index
+    puts "Camera index controller"
+    @images = Image.find(:all)
+    render
+  end
+
+  def new
+    Camera::take_picture(url_for :action => :camera_callback)
+    redirect :action => :index
+  end
+
+  def edit
+    Camera::choose_picture(url_for :action => :camera_callback)
+    redirect :action => :index
+  end
+
+  def camera_callback
+    if @params['status'] == 'ok'
+      #create image record in the DB
+      image = Image.new({'image_uri'=>@params['image_uri']})
+      image.save
+      puts "new Image object: " + image.inspect
+      WebView::refresh
+    end  
+    #reply on the callback
+    render :action => :ok, :layout => false
+  end
+      
+end
