f37a4eff9e9c8450ba7e08df8ec8a53e5a69072d
vtrv101 <vlad@vlads-macbook-pro.local>
2009-07-02 17:59:33 -0700

    Modifyed to test Alert api on iPhone

--- app/Alert/controller.rb
+++ app/Alert/controller.rb
@@ -4,27 +4,38 @@ class AlertController < Rho::RhoController
   @layout = :simplelayout
   
   def index
+    @flash = "Index page"
     render
   end
 
   def show_popup
+    @flash = "Show popup page"
     Alert.show_popup "Some message!"
     render :action => :index
   end
   
   def vibrate
+    @flash = "Vibrate page"
     Alert.vibrate
     render :action => :index
   end
   
   def vibrate_for_10sec
+    @flash = "Vibrate for 10 sec page"    
     Alert.vibrate 10000
     render :action => :index
   end
     
   def play_file
+    @flash = "Play file page"    
     Alert.play_file @params['file_name'], @params['media_type']
     render :action => :index    
   end
+  
+  def play_file_1
+    @flash = "Play file page"    
+    Alert.play_file @params['file_name']
+    render :action => :index    
+  end  
   
 end
