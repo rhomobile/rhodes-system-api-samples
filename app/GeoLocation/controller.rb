3b8cafb2ccde08ae1592ad9252c4a0597321b2dc
vtrv101 <vlad@vlads-macbook-pro-2.local>
2009-09-29 14:46:56 -0700

    added sample of iphone mapping widget

--- app/GeoLocation/controller.rb
+++ app/GeoLocation/controller.rb
@@ -7,5 +7,24 @@ class GeoLocationController < Rho::RhoController
     puts "GeoLocation index controller"
     render
   end
-      
+  
+  def show 
+    render :action => :show
+  end
+    
+  def showmap
+     puts @params.inspect
+     #pin color
+     map_params = {
+          :settings => {:map_type => "hybrid",:region => [@params['latitude'], @params['longitude'], 0.2, 0.2],
+                        :zoom_enabled => true,:scroll_enabled => true,:shows_user_location => false},
+          :annotations => [{:latitude => @params['latitude'], :longitude => @params['longitude'], :title => "Current location", :subtitle => ""},
+                           {:street_address => "Cupertino, CA 95014", :title => "Cupertino", :subtitle => "zip: 95014", :url => "/app/GeoLocation/show?city=Cupertino"},
+                           {:street_address => "Santa Clara, CA 95051", :title => "Santa Clara", :subtitle => "zip: 95051", :url => "/app/GeoLocation/show?city=Santa%20Clara"}]
+     }
+     puts map_params.inspect            
+     MapView.create map_params
+     redirect :action => :index
+  end
+        
 end
