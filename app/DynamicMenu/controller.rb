3c77faf88789d120c91df2467b3ac6347b2e251b
lars <larsburgess@gmail.com>
2009-12-06 22:31:07 -0800

    adding dynamic menu test

--- app/DynamicMenu/controller.rb
+++ app/DynamicMenu/controller.rb
@@ -4,20 +4,8 @@ class DynamicMenuController < Rho::RhoController
 
   # GET /DynamicMenu
   def index
-    render
-  end
-  
-  def menu_one
     @menu = { "Go Home" => :home, "Refresh" => :refresh, "Options" => :options, 
-                :separator => nil, "Log" => :log, "New Account" => "/app/Account/new" }
-    render :action => :menu_one
-  end
-  
-  def menu_two
-    render :action => :menu_two
-  end
-  
-  def menu_three
-    render :action => :menu_three
+                :separator => nil, "Log" => :log, "Go to Settings" => "/app/Settings" }
+    render
   end
 end
