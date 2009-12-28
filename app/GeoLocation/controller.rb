1881476c70bd26355fd9a1c7da4a28ebb0101351
Dmitry Moskalchuk <dm@crystax.net>
2009-12-28 05:11:12 +0300

    Allow to see BB native MapView recent changes

--- app/GeoLocation/controller.rb
+++ app/GeoLocation/controller.rb
@@ -11,19 +11,24 @@ class GeoLocationController < Rho::RhoController
   def show 
     render :action => :show
   end
-    
+
   def showmap
-     puts @params.inspect
-     #pin color
-     map_params = {
-          :settings => {:map_type => "hybrid",:region => [@params['latitude'], @params['longitude'], 0.2, 0.2],
-                        :zoom_enabled => true,:scroll_enabled => true,:shows_user_location => false},
-          :annotations => [{:latitude => @params['latitude'], :longitude => @params['longitude'], :title => "Current location", :subtitle => ""},
-                           {:street_address => "Cupertino, CA 95014", :title => "Cupertino", :subtitle => "zip: 95014", :url => "/app/GeoLocation/show?city=Cupertino"},
-                           {:street_address => "Santa Clara, CA 95051", :title => "Santa Clara", :subtitle => "zip: 95051", :url => "/app/GeoLocation/show?city=Santa%20Clara"}]
-     }
-     puts map_params.inspect            
-     MapView.create map_params
+     if System.get_property('platform') == 'Blackberry'
+       # Temporary fork for bb: just to be sure it's working. Will be changed to common interface soon
+       MapView.create(GeoLocation.latitude, GeoLocation.longitude, 4)
+     else
+       puts @params.inspect
+       #pin color
+       map_params = {
+            :settings => {:map_type => "hybrid",:region => [@params['latitude'], @params['longitude'], 0.2, 0.2],
+                          :zoom_enabled => true,:scroll_enabled => true,:shows_user_location => false},
+            :annotations => [{:latitude => @params['latitude'], :longitude => @params['longitude'], :title => "Current location", :subtitle => ""},
+                             {:street_address => "Cupertino, CA 95014", :title => "Cupertino", :subtitle => "zip: 95014", :url => "/app/GeoLocation/show?city=Cupertino"},
+                             {:street_address => "Santa Clara, CA 95051", :title => "Santa Clara", :subtitle => "zip: 95051", :url => "/app/GeoLocation/show?city=Santa%20Clara"}]
+       }
+       puts map_params.inspect            
+       MapView.create map_params
+     end
      redirect :action => :index
   end
         
