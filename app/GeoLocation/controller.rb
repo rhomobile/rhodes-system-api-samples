43f7c55babc7c5c04226b9fdf5314d07fba6e0f6
Dmitry Moskalchuk <dm@crystax.net>
2010-02-05 12:15:16 +0300

    Fix :show_user_location => true

--- app/GeoLocation/controller.rb
+++ app/GeoLocation/controller.rb
@@ -17,7 +17,7 @@ class GeoLocationController < Rho::RhoController
      #pin color
      map_params = {
           :settings => {:map_type => "hybrid",:region => [@params['latitude'], @params['longitude'], 0.2, 0.2],
-                        :zoom_enabled => true,:scroll_enabled => true,:shows_user_location => false},
+                        :zoom_enabled => true,:scroll_enabled => true,:shows_user_location => true},
           :annotations => [{:latitude => @params['latitude'], :longitude => @params['longitude'], :title => "Current location", :subtitle => ""},
                            {:street_address => "Cupertino, CA 95014", :title => "Cupertino", :subtitle => "zip: 95014", :url => "/app/GeoLocation/show?city=Cupertino"},
                            {:street_address => "Santa Clara, CA 95051", :title => "Santa Clara", :subtitle => "zip: 95051", :url => "/app/GeoLocation/show?city=Santa%20Clara"}]
