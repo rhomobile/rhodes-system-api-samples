cb8634d44e90a6dace43acc4a2a1df58359a61ac
genywind <evgeny@rhomobile.com>
2010-02-10 00:38:30 +0300

    add AsyncHttp test

---
+++ app/AsyncHttpTest/controller.rb
@@ -0,0 +1,41 @@
+require 'rho/rhocontroller'
+
+class AsyncHttpTestController < Rho::RhoController
+
+  #GET /AsyncHttpTest
+  def index
+
+    @@get_result = ""
+    AsyncHttp.get(
+      :url => 'http://www.apache.org/licenses/LICENSE-2.0',
+      :headers => {'Cookie2' => 'test'},
+      :callback => (url_for :action => :httpget_callback),
+      :callback_param => "" )
+      
+    render :action => :wait
+  end
+  
+  def get_res
+    @@get_result    
+  end
+  
+  def httpget_callback
+    puts "httpget_callback: #{@params}"
+
+    @@get_result = @params['body']
+    
+    WebView.navigate ( url_for :action => :show_result )
+  end
+
+  def show_result
+    render :action => :index, :back => '/app'
+  end
+    
+  def cancel_httpcall
+    AsyncHttp.cancel
+
+    @@get_result  = 'Request was cancelled.'
+    render :action => :index, :back => '/app'
+  end
+  
+end
