20a146ea0a22022f045df809b810f230a83c3a67
Lee Lundrigan <leelundrigan@gmail.com>
2010-04-23 16:17:54 -0700

    uiformdemo bug fixes

--- app/UIFormDemo/task.rb
+++
@@ -1,6 +0,0 @@
-# The model has already been created by the framework, and extends Rhom::RhomObject
-# You can add more methods here
-
-class Task
-  #add model specifc code here
-end
--- app/UIFormDemo/task_spec.rb
+++
@@ -1,7 +0,0 @@
-describe "Task" do
-  #this test always fails, you really should have tests!
-
-  it "should have tests" do
-    true.should == false
-  end
-end
