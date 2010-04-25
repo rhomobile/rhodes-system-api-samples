cf7bce1f30abbf6351edf00ada88b91bb4f9f79d
genywind <evgeny@rhomobile.com>
2010-04-25 12:51:44 +0400

    add Full screen test

--- app/DynamicMenu/controller.rb
+++ app/DynamicMenu/controller.rb
@@ -5,7 +5,8 @@ class DynamicMenuController < Rho::RhoController
   # GET /DynamicMenu
   def index
     @menu = { "Go Home" => :home, "Refresh" => :refresh, "Options" => :options, 
-                :separator => nil, "Log" => :log, "Go to Settings" => "/app/Settings" }
+                :separator => nil, "Log" => :log, "Go to Settings" => "/app/Settings",
+                "Full screen" => 'fullscreen' }
     render
   end
 end
