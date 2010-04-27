85285f5546d7db67c6ca60c4aec4b1e5c2a232d6
Dmitry Moskalchuk <dm@crystax.net>
2010-04-27 15:25:23 +0400

    NavBar test

---
+++ app/Navbar/controller.rb
@@ -0,0 +1,33 @@
+require 'rho/rhocontroller'
+
+class NavbarController < Rho::RhoController
+
+  @@this_page = '/app/Navbar'
+
+  def enable
+    NavBar.create :title => "NavBar test", :left => {:action => :back, :label => 'Back'}, :right => {:action => '/app/NativeBarTest', :label => "NativeBar"}
+    render :action => :index
+  end
+
+  def disable
+    NavBar.remove
+    render :action => :index
+  end
+
+=begin
+  def set_toolbar
+    toolbar = [
+      {:action => :back,    :icon => '/public/images/bar/back_btn.png'},
+      {:action => :forward, :icon => '/public/images/bar/forward_btn.png'},
+      {:action => :separator},
+      {:action => :home,    :icon => '/public/images/bar/home_btn.png'},
+      {:action => :refresh },
+      {:action => 'callback:' + url_for(:action => :callback), :label => 'Callback' },
+      {:action => :options}
+    ]
+    NativeBar.create(Rho::RhoApplication::TOOLBAR_TYPE, toolbar)
+    WebView.navigate @@this_page
+  end
+=end
+
+end
