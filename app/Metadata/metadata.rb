8b0e457e18193f6946212661a6b60215a62ff804
Brian Moore <brian@Brian-Moores-MacBook-Pro.local>
2010-06-07 16:46:48 -0700

    Add metadata tests

---
+++ app/Metadata/metadata.rb
@@ -0,0 +1,7 @@
+# The model has already been created by the framework, and extends Rhom::RhomObject
+# You can add more methods here
+class Metadata
+  include Rhom::PropertyBag
+  enable :sync
+  #add model specifc code here
+end
