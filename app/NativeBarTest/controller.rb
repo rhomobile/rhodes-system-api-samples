3b695e307f65675c2a6d3a8dc8384dcf2ed825d9
dmitrys <dmitrys@rhomobile.com>
2010-11-19 06:36:30 +0300

    add switch icons in toolbar test (Requestec)

--- app/NativeBarTest/controller.rb
+++ app/NativeBarTest/controller.rb
@@ -8,6 +8,11 @@ class NativeBarTestController < Rho::RhoController
     @@this_page = location
   end
 
+  def return_to_main
+     set_toolbar
+     #redirect :action => :index
+  end
+
   def set_no_bar
     save_location
     NativeBar.create(Rho::RhoApplication::NOBAR_TYPE, [])
@@ -72,5 +77,118 @@ class NativeBarTestController < Rho::RhoController
     puts "+++--- callback"
     WebView.navigate '/app'
   end
+
+
+  def nop
+  end
+
+  def switch_to_1
+    #save_location
+    toolbar = [
+      {:action => 'callback:' + url_for(:action => :nop),    :icon => '/public/images/bar/switch/btn_1_c.png', :colored_icon => true},
+      {:action => :separator},
+      {:action => url_for(:action => :switch_to_2),    :icon => '/public/images/bar/switch/btn_2.png'},
+      {:action => :separator},
+      {:action => url_for(:action => :switch_to_3),    :icon => '/public/images/bar/switch/btn_3.png'},
+      {:action => :separator},
+      {:action => url_for(:action => :switch_to_4),    :icon => '/public/images/bar/switch/btn_4.png'},
+      {:action => :separator},
+      {:action => url_for(:action => :switch_to_5),    :icon => '/public/images/bar/switch/btn_5.png'}
+    ]
+    NativeBar.create(Rho::RhoApplication::TOOLBAR_TYPE, toolbar)
+    #redirect :action => :switch1.erb
+    render :action => :switch1
+  end
+
+  def switch_to_2
+    #save_location
+    toolbar = [
+      {:action => url_for(:action => :switch_to_1),    :icon => '/public/images/bar/switch/btn_1.png'},
+      {:action => :separator},
+      {:action => 'callback:' + url_for(:action => :nop),    :icon => '/public/images/bar/switch/btn_2_c.png', :colored_icon => true},
+      {:action => :separator},
+      {:action => url_for(:action => :switch_to_3),    :icon => '/public/images/bar/switch/btn_3.png'},
+      {:action => :separator},
+      {:action => url_for(:action => :switch_to_4),    :icon => '/public/images/bar/switch/btn_4.png'},
+      {:action => :separator},
+      {:action => url_for(:action => :switch_to_5),    :icon => '/public/images/bar/switch/btn_5.png'}
+    ]
+    NativeBar.create(Rho::RhoApplication::TOOLBAR_TYPE, toolbar)
+    #redirect :action => :switch2.erb
+    render :action => :switch2
+  end
+
+  def switch_to_3
+    #save_location
+    toolbar = [
+      {:action => url_for(:action => :switch_to_1),    :icon => '/public/images/bar/switch/btn_1.png'},
+      {:action => :separator},
+      {:action => url_for(:action => :switch_to_2),    :icon => '/public/images/bar/switch/btn_2.png'},
+      {:action => :separator},
+      {:action => 'callback:' + url_for(:action => :nop),    :icon => '/public/images/bar/switch/btn_3_c.png', :colored_icon => true},
+      {:action => :separator},
+      {:action => url_for(:action => :switch_to_4),    :icon => '/public/images/bar/switch/btn_4.png'},
+      {:action => :separator},
+      {:action => url_for(:action => :switch_to_5),    :icon => '/public/images/bar/switch/btn_5.png'}
+    ]
+    NativeBar.create(Rho::RhoApplication::TOOLBAR_TYPE, toolbar)
+    #redirect :action => :switch3.erb
+    render :action => :switch3
+  end
+
+  def switch_to_4
+    #save_location
+    toolbar = [
+      {:action => url_for(:action => :switch_to_1),    :icon => '/public/images/bar/switch/btn_1.png'},
+      {:action => :separator},
+      {:action => url_for(:action => :switch_to_2),    :icon => '/public/images/bar/switch/btn_2.png'},
+      {:action => :separator},
+      {:action => url_for(:action => :switch_to_3),    :icon => '/public/images/bar/switch/btn_3.png'},
+      {:action => :separator},
+      {:action => 'callback:' + url_for(:action => :nop),    :icon => '/public/images/bar/switch/btn_4_c.png', :colored_icon => true},
+      {:action => :separator},
+      {:action => url_for(:action => :switch_to_5),    :icon => '/public/images/bar/switch/btn_5.png'}
+    ]
+    NativeBar.create(Rho::RhoApplication::TOOLBAR_TYPE, toolbar)
+    #redirect :action => :switch4.erb
+    render :action => :switch4
+  end
+
+  def switch_to_5
+    #save_location
+    toolbar = [
+      {:action => url_for(:action => :switch_to_1),    :icon => '/public/images/bar/switch/btn_1.png'},
+      {:action => :separator},
+      {:action => url_for(:action => :switch_to_2),    :icon => '/public/images/bar/switch/btn_2.png'},
+      {:action => :separator},
+      {:action => url_for(:action => :switch_to_3),    :icon => '/public/images/bar/switch/btn_3.png'},
+      {:action => :separator},
+      {:action => url_for(:action => :switch_to_4),    :icon => '/public/images/bar/switch/btn_4.png'},
+      {:action => :separator},
+      {:action => 'callback:' + url_for(:action => :nop),    :icon => '/public/images/bar/switch/btn_5_c.png', :colored_icon => true}
+    ]
+    NativeBar.create(Rho::RhoApplication::TOOLBAR_TYPE, toolbar)
+    #redirect :action => :switch5.erb
+    render :action => :switch5
+  end
+
+
+  def switch1
+    render :action => :switch1
+  end
+
+  def switch2
+    render :action => :switch2
+  end
+  def switch3
+    render :action => :switch3
+  end
+  def switch4
+    render :action => :switch4
+  end
+  def switch5
+    render :action => :switch5
+  end
+
 
 end
