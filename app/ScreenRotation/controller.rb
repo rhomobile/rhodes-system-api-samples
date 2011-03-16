f984841c58a6393c430a9489646a3323e2251fe7
dmitrys <dmitrys@rhomobile.com>
2011-03-16 18:04:31 +0300

    add set/unset screen rotation callback

--- app/ScreenRotation/controller.rb
+++ app/ScreenRotation/controller.rb
@@ -2,9 +2,22 @@ require 'rho/rhocontroller'
 
 class ScreenRotationController < Rho::RhoController
   @layout = :simplelayout
-  
-  def index
+  $callback_state = 'screen rotation callback is not setted'  
+
+  def set_callback
     System::set_screen_rotation_notification(url_for(:action => :screenRotateCallback), "" )
+    $callback_state = 'screen rotation callback is setted'  
+    redirect :action => :index
+  end
+
+  def unset_callback
+    System::set_screen_rotation_notification( nil, nil )
+    $callback_state = 'screen rotation callback is not setted'  
+    redirect :action => :index
+  end
+
+
+  def index
     render :back => '/app'  
   end
 
