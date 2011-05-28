12cf56b9920e5a6bd95fb7fcb4bc46704ef36d66
genywind <evgeny@rhomobile.com>
2011-05-28 09:53:04 +0400

    fix toolbar test

--- app/NativeToolbarTest/controller.rb
+++ app/NativeToolbarTest/controller.rb
@@ -28,18 +28,28 @@ class NativeToolbarTestController < Rho::RhoController
 
 def set_toolbar
     save_location
-    if defined?( RHO_WP7 )
-        iconpath = '/public/images/bar/gears.png'
-    end
+if !defined?( RHO_WP7 )      
     toolbar = [
       {:action => :back,    :icon => '/public/images/bar/back_btn.png'},
       {:action => :forward, :icon => '/public/images/bar/forward_btn.png'},
       {:action => :separator},
       {:action => :home,    :icon => '/public/images/bar/colored_btn.png', :colored_icon => true},
       {:action => :refresh },
-      {:action => 'callback:' + url_for(:action => :callback), :label => 'Callback', :icon => iconpath},
+      {:action => 'callback:' + url_for(:action => :callback), :label => 'Callback'},
       {:action => :options}
     ]
+else
+    iconpath = '/public/images/bar/gears.png'
+    toolbar = [
+      {:action => :back,    :icon => '/public/images/bar/back_btn.png'},
+      {:action => :forward, :icon => '/public/images/bar/forward_btn.png'},
+      {:action => :separator},
+      {:action => :home,    :icon => '/public/images/bar/colored_btn.png', :colored_icon => true},
+      {:action => :refresh },
+      {:action => 'callback:' + url_for(:action => :callback), :label => 'Callback' , :icon => iconpath },
+      {:action => :options}
+    ]
+end    
     #NativeBar.create(Rho::RhoApplication::TOOLBAR_TYPE, toolbar)
     Rho::NativeToolbar.create(toolbar)
     $tabbar_active = false
