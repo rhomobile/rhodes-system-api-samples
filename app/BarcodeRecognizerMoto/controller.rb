0bfa022c51b26abe2e89a8725ecdf91a501e15df
genywind <evgeny@rhomobile.com>
2011-12-22 20:29:56 +0400

    moto barcode: add multiscan sample

--- app/BarcodeRecognizerMoto/controller.rb
+++ app/BarcodeRecognizerMoto/controller.rb
@@ -31,12 +31,12 @@ class BarcodeRecognizerMotoController < Rho::RhoController
       status = @params['status']
       barcode = @params['barcode']
 
-      puts 'BarcodeRecognizer::take_callback !'
+      puts 'take_callback'
       puts 'status = '+status.to_s unless status == nil
       puts 'barcode = '+barcode.to_s unless barcode == nil
 
       if status == 'ok'
-           Alert.show_popup  ('Barcode['+recognized_code.to_s+']')  
+           Alert.show_popup  ('Barcode['+barcode.to_s+']')  
       elsif status == 'cancel'
            Alert.show_popup  ('Barcode taking was canceled !')  
       end
@@ -45,4 +45,42 @@ class BarcodeRecognizerMotoController < Rho::RhoController
       redirect :action => :index
   end
 
+  def multiscan
+    $barcodes = []
+    Barcode.enable( url_for(:action => :multi_callback), {})
+    redirect :action => :show_barcodes
+  end
+
+  def multi_callback
+      status = @params['status']
+      barcode = @params['barcode']
+
+      puts 'multi_callback'
+      puts 'status = '+status.to_s unless status == nil
+      puts 'barcode = '+barcode.to_s unless barcode == nil
+
+      $barcodes += [barcode] if status == 'ok'
+      
+      WebView.refresh
+  end
+  
+  def show_barcodes
+    render :back => 'callback:' + url_for(:action => :go_back)
+  end
+  
+  def go_back
+    Barcode.disable
+    WebView.navigate url_for(:action => :index)
+  end
+  
+  def start_scan
+    Barcode.start
+    redirect :action => :show_barcodes        
+  end
+
+  def stop_scan
+    Barcode.stop
+    redirect :action => :show_barcodes        
+  end
+     
 end
