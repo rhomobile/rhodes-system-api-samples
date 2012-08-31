f747dac4f7ac48c011ab7b2cdc1410f68f0c2fb3
Alex Epifanoff <alex.epifanoff@gmail.com>
2012-08-31 19:25:06 +0400

    network notification test

---
+++ app/NetworkStatus/controller.rb
@@ -0,0 +1,27 @@
+require 'rho/rhocontroller'
+
+class NetworkStatusController < Rho::RhoController
+  @layout = :simplelayout
+  $callback_state = 'network status callback is not setted'  
+
+  def set_callback
+    System::set_network_status_notify( url_for( :action => :networkStatusCallback), 20 )
+    $callback_state = 'network status callback is setted'  
+    redirect :action => :index
+  end
+
+  def unset_callback
+    System:: clear_network_status_notify()
+    $callback_state = 'network status callback is not setted'  
+    redirect :action => :index
+  end
+
+
+  def index
+    render :back => '/app'  
+  end
+
+  def networkStatusCallback
+    Alert.show_popup ("Network state changed from #{@params['prev_status']} to #{@params['current_status']}")    
+  end
+end
