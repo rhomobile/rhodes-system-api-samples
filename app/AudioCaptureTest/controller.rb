c56220475ef19b534f928136651a8fcecb7bd5e2
dmitrys <dmitrys@rhomobile.com>
2012-10-10 16:26:40 +0400

    add AudioCapture test

---
+++ app/AudioCaptureTest/controller.rb
@@ -0,0 +1,49 @@
+require 'rho/rhocontroller'
+require 'helpers/application_helper'
+require 'helpers/browser_helper'
+require 'audiocapture'
+
+class AudioCaptureTestController < Rho::RhoController
+  include BrowserHelper
+  include ApplicationHelper
+
+  @layout = 'AudioCaptureTest/layout'
+  
+  def index
+    render :back => '/app'
+  end
+
+
+  def sc_start
+
+      #AudioCapture::name = 'mysound.wav'
+      AudioCapture::destination = File.join(Rho::RhoApplication::get_user_path(), 'mysound.wav')
+      AudioCapture::duration = 5000
+      AudioCapture::audioSaveEvent = url_for(:action => :sc_callback)
+      AudioCapture::start
+
+  end
+
+
+  def sc_stop
+      AudioCapture::stop
+  end
+
+
+  def sc_cancel
+      AudioCapture::cancel
+  end
+
+
+  def sc_play
+      #Alert.play_file File.join(Rho::RhoApplication::get_user_path(), 'mysound.wav')
+      System.open_url(File.join(Rho::RhoApplication::get_user_path(), 'mysound.wav'))
+      #redirect :action => :index
+  end
+
+  def sc_callback
+      puts 'AudioCapture::callback    STATUS = ' +  @params['status']
+  end
+
+  
+end
