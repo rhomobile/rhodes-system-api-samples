3cf13e5bc7548ae33d16eb7a094cd2e7be39387f
Alexey Tikhvinsky <alexey@rhomobile.com>
2013-05-06 22:12:00 +0400

    Camera 4 sample

---
+++ app/Camera/controller.rb
@@ -0,0 +1,60 @@
+require 'rho/rhocontroller'
+require 'helpers/application_helper'
+require 'helpers/browser_helper'
+
+
+class CameraController < Rho::RhoController
+  include BrowserHelper
+  include ApplicationHelper
+
+  def index
+    Rho::Camera.enumerate url_for(:action => :enumCallback) unless $cameraList
+    render
+  end
+  
+  def camera
+    render :camera
+  end
+  
+  def enumCallback
+    puts @params.inspect
+    $cameraList = @params['result'] if (@params['result'])
+    Rho::WebView.navigate(url_for(:action => :index))
+  end
+  
+  def takePicture
+    options = {
+        :outputFormat => @params['outputFormat'],
+        :colorModel => @params['colorModel'],
+        :fileName => "#{Rho::Application.databaseBlobFolder}/sample",
+        :desiredWidth => $cameraSizeList[@params['size'].to_i]['width'].to_s,
+        :desiredHeight => $cameraSizeList[@params['size'].to_i]['height'].to_s}
+    options['saveToDeviceGallery'] = 'true' if (@params['target'] == 'gallery')
+    
+    $cameraList[@params['cameraIdx'].to_i].takePicture options, url_for(:action => :takeCallback)
+    render :index
+  end
+  
+  def systemApp
+    options = {:useSystemViewfinder => "true", :outputFormat => "image", :fileName => "#{Rho::Application.databaseBlobFolder}/sample"}
+    $cameraList.first.takePicture options, url_for(:action => :takeCallback)
+    render :index
+  end
+  
+  def choose
+    options = {:fileName => "#{Rho::Application.databaseBlobFolder}/sample"}
+    Rho::Camera.choosePicture options, url_for(:action => :takeCallback)
+    render :index
+  end
+  
+  def open
+    Rho::System.openUrl @params['url']
+    render :index
+  end
+  
+  def takeCallback
+    puts @params.inspect
+    $sample = @params['imageUri'] if (@params['imageUri'])
+    Rho::WebView.navigate(url_for(:action => :index))
+  end
+end
